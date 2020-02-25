56f387bf2721a712f6ea75da8b19524e55427452
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-02-24 21:52:40 -0600

    agregados y al parecer bien

---
+++ producto.js
@@ -0,0 +1,16 @@
+import Precio from "./precio.js"
+
+export default class Producto{
+    /**
+     * 
+     * @param {string} nombre 
+     * @param {Precio} precio 
+     */
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    getDescripcion(){
+        return(`${this.nombre} ${this.precio.getPrecio()}`);
+    }
+}

bada4acbc4f786f85d95792ee3c5ec9238088621
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-02-24 21:50:37 -0600

    correción de tiempo

--- tiempo.js
+++ tiempo.js
@@ -3,23 +3,33 @@ export default class Tiempo{
      * 
      * @param {number} hora 
      * @param {number} minutos 
-     * @param {string} periodo 
      */
-        constructor(hora, minutos, periodo){
+        constructor(hora, minutos){
             this.hora = hora;
             this.minutos = minutos;
-            this.periodo = periodo;
+            this.periodo = ["am","pm"];
         }
         getFormato12(){
-            return(`${this.hora}:${this.minutos} ${this.periodo}`);
+            if (this.hora>=12 && this.hora<=23){
+                if(this.hora==12){
+                    return (`${this.hora}:${this.minutos} ${this.periodo[1]}`);
+                }
+                else{
+                    var hora = this.hora-12;
+                }
+                
+                return (`${hora}:${this.minutos} ${this.periodo[1]}`);
+            }
+            else if (this.hora==24){
+    
+                return (`00:${this.minutos} ${this.periodo[0]}`);
+            }
+            else{
+                return (`${this.hora}:${this.minutos} ${this.periodo[0]}`);
         }
+    }
         getFormato24(){
             if(this.periodo === "pm"||this.periodo === "PM"||this.periodo === "Pm"){this.hora = this.hora + 12}
             return(`${this.hora}:${this.minutos}`);
-    
-            
         }
     }
-    let tiempo1 = new Tiempo(5, 21, "am");
-    console.log(tiempo1.getFormato12());
-    console.log(tiempo1.getFormato24());
